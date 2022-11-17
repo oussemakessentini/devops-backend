b30212062806fd7e3a26151a51502f8b53a3ed00
mariem-ben-ghozlen <58255315+mariem-ben-ghozlen@users.noreply.github.com>
2022-11-17 16:33:30 +0100

    update stock

--- src/main/java/tn/esprit/rh/achat/entities/Stock.java
+++ src/main/java/tn/esprit/rh/achat/entities/Stock.java
@@ -36,9 +36,7 @@ public class Stock implements Serializable {
 		this.qte = qte;
 		this.qteMin = qteMin;
 	}
-	public Stock() {
-		super();
-	}
+	
 	public Long getIdStock() {
 		return idStock;
 	}
